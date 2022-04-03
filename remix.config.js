718d27bcf35ebb29b84c69507de9f81b63fe8ca1
Zakery Clarke <30702789+zakerytclarke@users.noreply.github.com>
2022-04-02 22:50:35 -0600

    Add files via upload

---
+++ remix.config.js
@@ -0,0 +1,12 @@
+/**
+ * @type {import('@remix-run/dev/config').AppConfig}
+ */
+// eslint-disable-next-line no-undef
+module.exports = {
+  appDirectory: "app",
+  assetsBuildDirectory: "public/build",
+  publicPath: "/build/",
+  serverBuildDirectory: "build",
+  devServerPort: 8002,
+  ignoredRouteFiles: [".*"],
+};
